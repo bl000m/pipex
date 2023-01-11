7a3c21c9b29e3ef04ebcdd07d5675b2df1bfd3fa
Mathia Pagani <98359675+bl000m@users.noreply.github.com>
2023-01-11 10:19:07 +0100

    Add files via upload

---
+++ libft/ft_lstlast_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpagani <mpagani@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 09:58:24 by mpagani           #+#    #+#             */
+/*   Updated: 2022/12/27 16:27:59 by mpagani          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*ptr;
+
+	ptr = lst;
+	if (ptr == NULL)
+		return (NULL);
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+	return (ptr);
+}
